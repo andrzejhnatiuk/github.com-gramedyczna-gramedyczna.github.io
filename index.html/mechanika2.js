d393457c6356b1fb49cd289dd4560f3d921138a5
Andrzej Hnatiuk <andrzejhnatiuk72@gmail.com>
2024-03-02 16:31:34 +0100

    ut

--- index.html/mechanika2.js
+++ index.html/mechanika2.js
@@ -7,5 +7,67 @@ const wordList = [
         word: "azytromycyna",
         hint: "Makrolid"
     },
+    {
+        word: "penicylina",
+        hint: "Pierwszy antybiotyk"
+    },
+    {
+        word: "fleming",
+        hint: "Nazwisko lekarza który odkrył pierwszy antybiotyk"
+    },
+    {
+        word: "transpeptydaze",
+        hint: "Beta laktamy hamują ..., a tym samym syntezy ściany komórkowej bakterii"
+    },
+    {
+        word: "ampicylina",
+        hint: "Aminowa penicylina o poszerzonym zakresie działania"
+    },
+    {
+        word: "piperacylina",
+        hint: "Acyloaminowa penicylina o poszerzonym zakresie działania"
+    },
+    {
+        word: "sulbaktam",
+        hint: "Inhibitor β-laktamaz"
+    },
+    
+    {
+        word: "amoksycylina",
+        hint: "preferowana droga DOUSTNA"
+    },
+    {
+        word: "wysypka",
+        hint: "u pacjentów z mononukleozą po antybiotyku"
+    },
+    {
+        word: "mezlocylina",
+        hint: "silne działanie na ENTEROKOKI"
+    },
+    {
+        word: "piperacylina",
+        hint: "silne działanie na Pseudomonas"
+    },
+    {
+        word: "nerki",
+        hint: "Eliminacja większości cefalosporyn zachodzi przez..."
+    },
+    {
+        word: "imipenem",
+        hint: "BARDZO SZEROKI zakres działania"
+    },
+    
+    {
+        word: "wankomycyna",
+        hint: "Szczególnie na gronkowce"
+    },
+    {
+        word: "fosfomycyna",
+        hint: "Dobrze przenika do tkanek, w tym także DO KOŚCI"
+    },
+    {
+        word: "streptomycyna",
+        hint: "PIERWSZY antybiotyk aminoglikozydowy"
+    }
     
 ];
